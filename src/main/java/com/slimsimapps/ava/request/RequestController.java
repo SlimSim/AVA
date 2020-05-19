d3c06b969f3d9c0543c4f3053cb682efcab831fa
Slim Sim <slimsimapps@gmail.com>
2020-05-19 19:01:30 +0200

    removing demo code

--- src/main/java/com/slimsimapps/ava/request/RequestController.java
+++ src/main/java/com/slimsimapps/ava/request/RequestController.java
@@ -1,11 +1,8 @@
 package com.slimsimapps.ava.request;
 
-import com.slimsimapps.ava.Greeting;
-import com.slimsimapps.ava.participant.Participant;
 import com.slimsimapps.ava.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 
