cc861fdc7ab6aaa24bccd8bc1e33f09a1a858ce8
Slim Sim <slimsimapps@gmail.com>
2020-10-20 14:12:17 +0200

    adding BadLog logs and websocket extra handshake handler

--- src/main/java/com/slimsimapps/ava/WebSocketConfig.java
+++ src/main/java/com/slimsimapps/ava/WebSocketConfig.java
@@ -1,24 +1,40 @@
 package com.slimsimapps.ava;
 
+import com.slimsimapps.ava.badlog.BadLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Autowired
+    BadLogService log;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        log.a(config);
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+        log.o(config);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        log.a(registry);
+        //registry.addEndpoint("/gs-guide-websocket").withSockJS();
+
+        registry.addEndpoint("/gs-guide-websocket")
+        .setHandshakeHandler(new DefaultHandshakeHandler(new TomcatRequestUpgradeStrategy()))
+        .withSockJS();
+
+        log.o(registry);
     }
 
 }
