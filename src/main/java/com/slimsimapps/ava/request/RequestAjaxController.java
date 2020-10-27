9982555739488db402a8d2476458301f6cf79cd5
Slim Sim <slimsimapps@gmail.com>
2020-10-27 11:17:23 +0100

    adding ajax endpoints for request updates

--- src/main/java/com/slimsimapps/ava/request/RequestAjaxController.java
+++ src/main/java/com/slimsimapps/ava/request/RequestAjaxController.java
@@ -1,14 +1,19 @@
 package com.slimsimapps.ava.request;
 
 import com.slimsimapps.ava.badlog.BadLogService;
+import com.slimsimapps.ava.meeting.Meeting;
+import com.slimsimapps.ava.participant.Participant;
 import com.slimsimapps.ava.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
-@Controller
-public class RequestController {
+@RestController
+public class RequestAjaxController {
 
     @Autowired
     BadLogService log;
@@ -16,10 +21,22 @@ public class RequestController {
     @Autowired
     ParticipantService participantService;
 
-    @MessageMapping("/request")
-    public void request(Request request) throws Exception {
+    @PostMapping( "/ajax/request" )
+    public List<Participant> request(@RequestBody Request request) throws Exception {
+        log.a( request );
+        participantService.setParticipantRequest( request );
+        Meeting meeting = participantService.getParticipant( request.getParticipantId() ).getMeeting();
+        log.o();
+        return participantService.getSpeakerQue( meeting.getId() );
+    }
+
+    @PostMapping( "/ajax/singleRequest" )
+    public Participant singleRequest(@RequestBody Request request) throws Exception {
         log.a(request);
         participantService.setParticipantRequest( request );
+        Participant p = participantService.getParticipant( request.getParticipantId() );
+        log.o(p);
+        return p;
     }
 
 }
